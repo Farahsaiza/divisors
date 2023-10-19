a089983678c2d07e32c00a83b197c8d12539bfce
Farahsaiza <farahsaiza@gmail.com>
2023-10-19 08:00:07 +0100

    Update diviseurs.py

--- diviseurs.py
+++ diviseurs.py
@@ -1,10 +1,12 @@
 N=int(input("enter a number"))
+Tab=[]
 i=1
 S=0
 while i<=N:
     D= N// i
     if N % i==0 :
-        print(D)
+        Tab.append(D)
         S=S+1
     i=i+1
 print("the number of divisors is:" ,   S)
+print("divisors are :" ,  Tab)
